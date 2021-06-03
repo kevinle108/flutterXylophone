651de22a1fc051c0207ba356e277bade148854ae
Kevin Le <kevinle108@gmail.com>
2021-06-02 23:31:12 -0400

    refactored to widget return function

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,33 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  List<Color> colors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.teal,
+    Colors.blue,
+    Colors.purple,
+  ];
+
+
+
+  Widget createMusicButton(int buttonNum) {
+    return TextButton(
+      onPressed: () {
+        final AudioCache player = AudioCache();
+        player.play('note${buttonNum+1}.wav');
+      },
+      child: Container(
+        height: 50,
+        width: 100,
+        color: colors[buttonNum],
+      ),
+    );
+  }
+
+  int buttonNum = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,85 +39,92 @@ class XylophoneApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
-              TextButton(
-                onPressed: () {
-                  final AudioCache player = AudioCache();
-                  player.play('note1.wav');
-                },
-                child: Container(
-                  height: 50,
-                  width: 100,
-                  color: Colors.red,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  final AudioCache player = AudioCache();
-                  player.play('note2.wav');
-                },
-                child: Container(
-                  height: 50,
-                  width: 100,
-                  color: Colors.orange,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  final AudioCache player = AudioCache();
-                  player.play('note3.wav');
-                },
-                child: Container(
-                  height: 50,
-                  width: 100,
-                  color: Colors.yellow,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  final AudioCache player = AudioCache();
-                  player.play('note4.wav');
-                },
-                child: Container(
-                  height: 50,
-                  width: 100,
-                  color: Colors.green,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  final AudioCache player = AudioCache();
-                  player.play('note5.wav');
-                },
-                child: Container(
-                  height: 50,
-                  width: 100,
-                  color: Colors.teal,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  final AudioCache player = AudioCache();
-                  player.play('note6.wav');
-                },
-                child: Container(
-                  height: 50,
-                  width: 100,
-                  color: Colors.blue,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  final AudioCache player = AudioCache();
-                  player.play('note7.wav');
-                },
-                child: Container(
-                  height: 50,
-                  width: 100,
-                  color: Colors.purple,
-                ),
-              ),
 
-            ],
+              createMusicButton(0),
+              createMusicButton(1),
+              createMusicButton(2),
+              createMusicButton(3),
+              createMusicButton(4),
+              createMusicButton(5),
+              createMusicButton(6),
+              // TextButton(
+              //   onPressed: () {
+              //     final AudioCache player = AudioCache();
+              //     player.play('note1.wav');
+              //   },
+              //   child: Container(
+              //     height: 50,
+              //     width: 100,
+              //     color: Colors.red,
+              //   ),
+              // ),
+              // TextButton(
+              //   onPressed: () {
+              //     final AudioCache player = AudioCache();
+              //     player.play('note2.wav');
+              //   },
+              //   child: Container(
+              //     height: 50,
+              //     width: 100,
+              //     color: Colors.orange,
+              //   ),
+              // ),
+              // TextButton(
+              //   onPressed: () {
+              //     final AudioCache player = AudioCache();
+              //     player.play('note3.wav');
+              //   },
+              //   child: Container(
+              //     height: 50,
+              //     width: 100,
+              //     color: Colors.yellow,
+              //   ),
+              // ),
+              // TextButton(
+              //   onPressed: () {
+              //     final AudioCache player = AudioCache();
+              //     player.play('note4.wav');
+              //   },
+              //   child: Container(
+              //     height: 50,
+              //     width: 100,
+              //     color: Colors.green,
+              //   ),
+              // ),
+              // TextButton(
+              //   onPressed: () {
+              //     final AudioCache player = AudioCache();
+              //     player.play('note5.wav');
+              //   },
+              //   child: Container(
+              //     height: 50,
+              //     width: 100,
+              //     color: Colors.teal,
+              //   ),
+              // ),
+              // TextButton(
+              //   onPressed: () {
+              //     final AudioCache player = AudioCache();
+              //     player.play('note6.wav');
+              //   },
+              //   child: Container(
+              //     height: 50,
+              //     width: 100,
+              //     color: Colors.blue,
+              //   ),
+              // ),
+              // TextButton(
+              //   onPressed: () {
+              //     final AudioCache player = AudioCache();
+              //     player.play('note7.wav');
+              //   },
+              //   child: Container(
+              //     height: 50,
+              //     width: 100,
+              //     color: Colors.purple,
+              //   ),
+              // ),
+          ]
           ),
         ),
       ),
